c849c37b084f3a21ff05cf6e25a6af6ae3d2b5df
daluoran <star161@yeah.net>
2026-09-11 18:10:10 +0800

    Update jable.py

--- jable.py
+++ jable.py
@@ -11,7 +11,7 @@ class Spider(Spider):
 
     def init(self, extend=""):
         self.siteUrl = "https://jable.tv"
-                self.headers = {
+        self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
             "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
