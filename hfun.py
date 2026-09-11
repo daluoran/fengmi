5fe545ad329a0d3d46503b147f888bb09f6df289
daluoran <star161@yeah.net>
2026-09-11 17:40:13 +0800

    Add files via upload

---
+++ hfun.py
@@ -0,0 +1,37 @@
+{
+  "sites": [
+    {
+      "key": "py_huangguo",
+      "name": "黄果短剧[py]",
+      "type": 3,
+      "api": "https://gh-proxy.com/https://raw.githubusercontent.com/daluoran/fengmi/main/huangguo.py",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 0,
+      "ext": "direct=1"
+    },
+    {
+      "key": "Hanime",
+      "name": "Hanime[py]",
+      "type": 3,
+      "api": "https://gh-proxy.com/https://raw.githubusercontent.com/daluoran/fengmi/main/Hanime.py",
+      "searchable": 1,
+      "recordable": 0,
+      "timeout": 60,
+      "ext": "proxy",
+      "style": {
+        "type": "rect",
+        "ratio": 0.68
+      }
+    },
+    {
+      "key": "py_jable",
+      "name": "Jable[py]",
+      "type": 3,
+      "api": "https://gh-proxy.com/https://raw.githubusercontent.com/daluoran/fengmi/main/jable.py",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    }
+  ]
+}
